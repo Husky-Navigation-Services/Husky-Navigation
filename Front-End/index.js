670d6186a83d94f440a7cc3c5159916ab2a03a52
Amit Ferman <amitferman@gmail.com>
2020-11-25 00:34:39 -0800

    Sidebar Slides and Map Works!

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,9 +1,20 @@
 var mymap = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo( mymap );
+
+document.getElementById("logo").addEventListener("click", toggleSideBar);
+
+function toggleSideBar() {
+    var sidebarLeft = document.getElementById("sidebarLeftId");
+    if (sidebarLeft.style.width == "0px") {
+        sidebarLeft.style.opacity = 1;
+        sidebarLeft.style.width = "288px";
+    } else {
+        sidebarLeft.style.opacity = 0;
+        sidebarLeft.style.width = "0px";
+    }
+    
+  
+}
