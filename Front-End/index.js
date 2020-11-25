d3e8e7abb7b37422827b5ab9b01623d38fb6a052
Amit Ferman <amitferman@gmail.com>
2020-11-25 14:01:07 -0800

    Styling and Input Functionality

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,12 +1,40 @@
+
+// Initialize Map
 var mymap = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: ['a','b','c']
 }).addTo( mymap );
 
-document.getElementById("logo").addEventListener("click", toggleSideBar);
 
-function toggleSideBar() {
+
+// Add Event Listeners
+var fromElement = document.getElementById("from");
+var toElement = document.getElementById("to");
+document.getElementById("logo").addEventListener("click", toggleContent);
+document.getElementById("startingPointsId").addEventListener("change", updateStart);
+document.getElementById("destinationsId").addEventListener("change", updateDest);
+document.getElementById("navBtn").addEventListener("click", tryNav);
+
+function updateStart() {
+    fromElement.innerHTML = this.value;
+}
+
+function updateDest() {
+    toElement.innerHTML = this.value;
+}
+
+function tryNav() {
+    var from = fromElement.innerHTML;
+    var to = toElement.innerHTML;
+    if (navPossible(from, to)) {
+        nav(from, to);
+    } else {
+        alert("Invalid. Try again.")
+    }
+}
+
+function toggleContent() {
     var sidebarLeft = document.getElementById("sidebarLeftId");
     var horizontalBar = document.getElementById("horizontalBarId");
     if (sidebarLeft.style.width == "0px") {
@@ -18,6 +46,13 @@ function toggleSideBar() {
         sidebarLeft.style.width = "0px";
         horizontalBar.style.width = "calc(100% - 90px)";
     }
-    
-  
+}
+
+// Navigation
+function navPossible(from, to) {
+    return from != "Select Starting Point" && to != "Select Destination" && to != from;
+}
+
+function nav() {
+    alert("Navigating...");
 }
