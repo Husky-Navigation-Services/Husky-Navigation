dddaa4028b4c630da5d97b3b0b9ebd3b7d337fd4
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2020-11-25 14:02:32 -0800

    Update and rename test.java to Test.java

---
+++ Back-End/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        // TBD based on Back-end Modules
+      
+        // Front-end will be tested by visual input in contrast to the
+        // back-end. Review phase will consist of user feedback.
+    }
+}
