2bda1ed938754982ef7e3b754621536b7ba7c329
Amit Ferman <amitferman@gmail.com>
2020-11-25 23:09:23 -0800

    Minor Changes

--- Front-End/index.js
+++ Front-End/index.js
@@ -79,5 +79,4 @@ function setNavView(coord1, coord2) {
     var mid = [(coord1[0] + coord2[0]) / 2, (coord1[1] + coord2[1]) / 2];
     mymap.setView(mid);
     mymap.fitBounds([coord1, coord2]);
-
 }
