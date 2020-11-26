a584627a582a352a84df88878103affd71cdb87d
Amit Ferman <amitferman@gmail.com>
2020-11-26 13:31:29 -0800

    Merge branch 'main' of https://github.com/Husky-Navigation-Services/Husky-Navigation into main

--- Back-End/Test.java
+++ Back-End/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        // TBD based on Back-end Modules
+        // TBD based on Back-end Module Architecture
       
         // Front-end will be tested by visual input in contrast to the
         // back-end. Review phase will consist of user feedback.
