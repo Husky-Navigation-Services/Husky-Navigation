965592e198fdd0e71b1b5f883f7accb304d033a2
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2020-11-26 11:33:03 -0800

    Update Parser.java

--- Back-End/Parser.java
+++ Back-End/Parser.java
@@ -1,3 +1,10 @@
-public class Parser{
+public class Parser {
+    public Parser() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
     
+    public Set<Node> parse() { // given a map area, returns a set of nodes
+        // or some other similar data structure
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
