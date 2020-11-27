6e5a90287707bff441e4282695acda598ef01995
defenestra <defenestra@udubminecraft.com>
2020-11-26 17:23:36 -0800

    opens a port, added input and output

--- Back-End/Server.java
+++ Back-End/Server.java
@@ -1,7 +1,33 @@
 import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 public class Server {
     public Server(Parser parse, Set<Node> nodes) {
     
+    }
+
+    public static void main(String[] args) throws Exception {
+        start();
+    }
+
+    public void start() {
+        new Thread(() -> {
+            try {
+                ServerSocket server = new ServerSocket(8080);
+                Socket socket = server.accept();
+                in = new DateInputStream(socket.getInputStream());
+                out = new DateOutputStream(socket.getOutputStream());
+                while (true) {
+                    String command = in.readUTF();
+                    // call a back-end method to process the command
+                    // output something
+                }
+            }
+            catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }).start();
     }
 }
