ece894e4472907bafb3528595a0dc2fad7b0b8b0
defenestra <defenestra@udubminecraft.com>
2020-11-27 13:22:13 -0800

    squashed bugs

--- Back-End/Server.java
+++ Back-End/Server.java
@@ -1,7 +1,6 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
-import java.lang.*;
 
 public class Server {
     public Server(Parser parse, Set<Node> nodes) {
@@ -12,13 +11,13 @@ public class Server {
         start();
     }
 
-    public void start() {
+    public static void start() {
         new Thread(() -> {
             try {
                 ServerSocket server = new ServerSocket(8080);
                 Socket socket = server.accept();
-                in = new DateInputStream(socket.getInputStream());
-                out = new DateOutputStream(socket.getOutputStream());
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 while (true) {
                     String command = in.readUTF();
                     // call a back-end method to process the command
