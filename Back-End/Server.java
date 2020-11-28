39fddbaca055c338a26a3bc241275c52f261188d
defenestra <defenestra@udubminecraft.com>
2020-11-27 23:11:20 -0800

    changed port

--- Back-End/Server.java
+++ Back-End/Server.java
@@ -14,7 +14,7 @@ public class Server {
     public static void start() {
         new Thread(() -> {
             try {
-                ServerSocket server = new ServerSocket(8080);
+                ServerSocket server = new ServerSocket(8000);
                 Socket socket = server.accept();
                 System.out.println("server connected");
                 DataInputStream in = new DataInputStream(socket.getInputStream());
