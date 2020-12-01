51e1343e3df555ba69c4a2994d5e926da7b4d999
Amit Ferman <amitferman@gmail.com>
2020-11-30 17:54:37 -0800

    Fix

--- Front-End/index.js
+++ Front-End/index.js
@@ -230,7 +230,7 @@ function setNavView(coord1, coord2) {
 function isElementCentered (el, holder) {
     const { y } = el.getBoundingClientRect()
     const holderRect = holder.getBoundingClientRect()
-    if (y <= holderRect.top + holderRect.height - 60 && y >= holderRect.top + 20) {
+    if (y <= holderRect.top + holderRect.height - 50 && y >= holderRect.top + 40) {
         return true;
     } else {
         return false;
