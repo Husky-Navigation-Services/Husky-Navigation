f2045c46465e2a1b4c4c3481dc380c993fc33527
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2020-12-01 22:18:47 -0800

    Added Rain Check Feature

--- Front-End/index.js
+++ Front-End/index.js
@@ -297,6 +297,38 @@ function setNavView(coord1, coord2) {
     mymap.fitBounds([coord1, coord2]);
 }
 
+function weatherBalloon( cityID ) {
+    var key = '{yourkey}';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+  
+  window.onload = function() {
+    weatherBalloon( 6167865 );
+  }
+
+function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	var description = d.weather[0].description;
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+	
+	if( description.indexOf('rain') > 0 ) {
+        isRain.src = "RainIcon.jpg";
+    } else {
+        isRain.src = "NoRainIcon.png";
+    }
+}
+
 ////////////////////////////
 // Scrolling Tools
 ////////////////////////////
