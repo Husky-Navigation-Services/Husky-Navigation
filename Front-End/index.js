3bfb3b48777c775b2c3434078904d92a079f1dab
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2020-12-01 22:32:45 -0800

    Weather Feature Updates

--- Front-End/index.js
+++ Front-End/index.js
@@ -297,10 +297,11 @@ function setNavView(coord1, coord2) {
     mymap.fitBounds([coord1, coord2]);
 }
 
+// Gathers the weather data by using a open weather API.
 function weatherBalloon( cityID ) {
     var key = '{yourkey}';
     fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
-    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(resp) {return resp.json() }) // Convert data to json
     .then(function(data) {
       console.log(data);
     })
@@ -309,20 +310,12 @@ function weatherBalloon( cityID ) {
     });
   }
   
-  window.onload = function() {
-    weatherBalloon( 6167865 );
-  }
+    window.onload = function() {
+    weatherBalloon( 5809844 );
+}
 
-function drawWeather( d ) {
-	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
-	var description = d.weather[0].description;
-	
-	document.getElementById('description').innerHTML = description;
-	document.getElementById('temp').innerHTML = celcius + '&deg;';
-	document.getElementById('location').innerHTML = d.name;
-	
-	if( description.indexOf('rain') > 0 ) {
+function isRain( d ) {
+	if( d.weather[0].description.indexOf('rain') > 0 ) {
         isRain.src = "RainIcon.jpg";
     } else {
         isRain.src = "NoRainIcon.png";
