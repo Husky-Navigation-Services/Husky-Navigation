325170fcf19a08448c7dfbbfc4b7c4a09fdda1de
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2020-12-10 22:53:57 -0800

    Maven Server

---
+++ Server/huskynavigationserver/src/main/java/com/uw/huskynavigation/App.java
@@ -0,0 +1,13 @@
+package com.uw.huskynavigation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Server/huskynavigationserver/src/main/java/com/uw/huskynavigation/coordinateFlipper.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class coordinateFlipper {
+    public static void main(String[] args) throws FileNotFoundException{
+        File nodes = new File("./Back-End/nodes.txt");
+        File reversedNodes = new File("reversedNodes.txt");
+        reverseCoordinates(new Scanner(nodes), new PrintStream(reversedNodes));
+    }
+
+    private static void reverseCoordinates(Scanner input, PrintStream output) {
+        if (!input.hasNext()) {
+            return;
+        }
+        String first = input.next();
+        String second = input.next();
+        String name = input.next();
+        output.print(second + " " + first + " " + name + " ");
+        reverseCoordinates(input, output);
+    }
+}
