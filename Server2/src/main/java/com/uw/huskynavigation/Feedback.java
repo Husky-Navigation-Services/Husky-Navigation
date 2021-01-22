e36e592a6104cf13db7f0c558dfea7c736e44721
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2021-01-21 18:28:52 -0800

    Delete Feedback.java

--- Server2/src/main/java/com/uw/huskynavigation/Feedback.java
+++
@@ -1,7 +0,0 @@
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-               return new PasswordAuthentication("huskynavigationfeedback", System.getenv("GMAIL_PASSWORD"));
-            }
-        });
-        context.getLogger().info("Entering email stage.");
-        try {
