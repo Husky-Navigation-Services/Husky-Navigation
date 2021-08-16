4ab49c66f0ac43ee2553987fe9526b042896cd4f
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2021-08-16 16:56:28 -0700

    Maintain HTPPS Server

--- Server/src/main/java/com/uw/huskynavigation/Edge.java
+++ Server/src/main/java/com/uw/huskynavigation/Edge.java
@@ -72,26 +72,22 @@ public class Edge {
 
     private double distance(double lat1, double lon1, double lat2, double lon2) {
         double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
-        return (dist);
-      }
+        double distance = Math.sin(degrees2radians(lat1)) * Math.sin(degrees2radians(lat2)) 
+                + Math.cos(degrees2radians(lat1)) * Math.cos(degrees2radians(lat2)) 
+                * Math.cos(degrees2radians(theta));
+        distance = Math.acos(distance);
+        distance = radians2degrees(distance);
+        distance = distance * 60 * 1.1515;
+        return (distance);
+    }
+
+    // Function converts decimal degrees to radians.
+    private double degrees2radians(double degrees) {
+        return (degrees * Math.PI / 180.0);
+    }
       
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      /*::  This function converts decimal degrees to radians             :*/
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      private double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-      }
-      
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      /*::  This function converts radians to decimal degrees             :*/
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      private double rad2deg(double rad) {
-        return (rad * 180.0 / Math.PI);
-      }
-      
+    // Function converts radians to decimal degrees.
+    private double radians2degrees(double radians) {
+        return (radians * 180.0 / Math.PI);
+    }  
 }
-// 1 <-- DO NOT DELETE THE DAMN 1
